8b85474eb940316c43c7e31386c6a012bbb2664e
Dany Harmer <danyharmor@gmail.com>
2025-12-23 11:12:02 +0300

    feat: add bearer token authentication

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ app.add_middleware(
         "http://127.0.0.1:5501",
         "http://localhost:5501",
         "https://hdlgame.netlify.app",
+        "https://*.netlify.app",
         "https://gamelabhdl.netlify.app"
     ],
     allow_credentials=True,
